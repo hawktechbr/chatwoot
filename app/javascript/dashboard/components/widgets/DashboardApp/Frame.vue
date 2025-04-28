11646a6698875d6ca5cebbb37c4a712facda8979
hawktechbr <47502610+hawktechbr@users.noreply.github.com>
2025-04-28 10:59:43 -0300

    Update Frame.vue
    Add clipboard-write permission to iframe

--- app/javascript/dashboard/components/widgets/DashboardApp/Frame.vue
+++ app/javascript/dashboard/components/widgets/DashboardApp/Frame.vue
@@ -98,6 +98,7 @@ export default {
         v-if="configItem.type === 'frame' && configItem.url"
         :id="getFrameId(index)"
         :src="configItem.url"
+        :allow="`clipboard-write ${configItem.url}`"
         @load="() => onIframeLoad(index)"
       />
     </div>
